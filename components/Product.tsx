0be666b4b19fb48047e46767395eed4c534f1581
rvishakhs <rvishakhs@gmail.com>
2022-12-06 23:55:52 +0000

    developing product component

--- components/Product.tsx
+++ components/Product.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import vegetarian from "./assets/vegetarian.png"
 import chilli from "./assets/chilli.png"
 import { urlFor } from '../sanity'
+import { useState } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
 
 interface Props {
     product : products
@@ -10,6 +12,20 @@ interface Props {
 
 
 function Product({product}: Props) {
+
+
+
+
+    const [size, setsize] = useState(0)
+
+
+    let colors = [
+        { id: "0", name: 'Medium'},
+        { id: "1", name: 'Small'},
+        { id: "2", name: 'Big' },
+
+      ];
+
     console.log(product)
     return (
     <div className="flex w-[280px] flex-col select-none space-y-2 rounded-xl bg-[#E2DFD2] p-2 md:h-[400px] md:w-[300px] md:p-2">
@@ -54,7 +70,25 @@ function Product({product}: Props) {
             </div>
 
         </div>
+        <div className='px-4 py-1 flex items-center space-x-2'>
+            <p className='font-bold text-lg '>$ {product.price[size]}</p>
+            <div>
+                <Dropdown autoClose={true} >
+                    <Dropdown.Toggle  id="dropdown-autoclose-true">
+                        Select Size
+                    </Dropdown.Toggle>
 
+                    <Dropdown.Menu className='flex flex-col gap-1 bg-white p-2 font-medium text-xs'>
+                        <Dropdown.Item onClick={() => setsize(1)}>Small</Dropdown.Item>
+                        <Dropdown.Item onClick={() => setsize(0)}>Medium</Dropdown.Item>
+                        <Dropdown.Item onClick={() => setsize(2)}>Large</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                </div>
+        </div>
+
+{/* 
+        onChange={(nextValue) => setsize(nextValue)} */}
     </div>       
     )
   }
